bce4cec9812f7c916ed1506cc8d1048c1b2da73d
Gonzalo Gabriel Perez <zalitoar@gmail.com>
2019-06-29 17:27:24 -0300

    Cambios en seed-layers y creación de set-styles

--- seed-layers.sh
+++ seed-layers.sh
@@ -4,28 +4,43 @@
 printf "\nFor reading properly the input file, please add a blank line at its end.\n\n"
 
 # import arguments
-source ./config-seed.sh
+source ./config.env
+
+debug=false
+date=$(date +%F-%H_%M)
+dataDir='data/'"$host"'/cache/'"$date"'/'
+mkdir -p $dataDir
 
 # makes an URL for sending the request with the arguments in config file
-baseSeedUrl="$host"'/geoserver/gwc/rest/seed/'
+baseSeedUrl='http://'"$host"'/geoserver/gwc/rest/seed/'
 
 # Read a list and requests cache operations by REST for certain layers
 
-while IFS=';' read -r layerName srs minx miny maxx maxy zoomStart zoomStop format type threadCount endline
+while IFS=';' read -r workspace layerName srs minx miny maxx maxy zoomStart zoomStop format type threads eol
 do
+	workDir="$dataDir""$layerName"'/'
+    mkdir -p $workDir
 
 ## Cache creation
+
+	# adds abstract xml tag if not empty    
+    if [ ! -z "$workspace" ]
+    then
+        xmlWorkspace="$workspace"':'
+    else
+        xmlWorkspace=''
+    fi
 
 	# seed url
 	# EVALUAR SI LA CAPA ESTA O NO EN WORKSPACE
 	restSeedUrl="$baseSeedUrl""$workspace""$layerNamePrefix""$layerName"'.xml'
 
 	# creates a string with layer's parameters in XML
-	seedXml='<seedRequest><name>'"$workspace"':'"$layerNamePrefix""$layerName"'</name><bounds><coords><double>'"$minx"'</double><double>'"$miny"'</double><double>'"$maxx"'</double><double>'"$maxy"'</double></coords></bounds><srs><number>'"$srs"'</number></srs><zoomStart>'"$zoomStart"'</zoomStart><zoomStop>'"$zoomStop"'</zoomStop><format>'"$format"'</format><type>'"$type"'</type><threadCount>'"$threadCount"'</threadCount></seedRequest>'
+	seedXml='<seedRequest><name>'"$xmlWorkspace""$layerNamePrefix""$layerName"'</name><bounds><coords><double>'"$minx"'</double><double>'"$miny"'</double><double>'"$maxx"'</double><double>'"$maxy"'</double></coords></bounds><srs><number>'"$srs"'</number></srs><zoomStart>'"$zoomStart"'</zoomStart><zoomStop>'"$zoomStop"'</zoomStop><format>'"$format"'</format><type>'"$type"'</type><threadCount>'"$threads"'</threadCount></seedRequest>'
 	# stores the xml in a temporary file
-	echo $seedXml > seed.xml
+	echo $seedXml > "$workDir""$layerName".xml
 	# creates cache for current layer
-	curl -v -u $gsUser -XPOST -H "Content-type: text/xml" -T seed.xml $restSeedUrl
+	curl -v -u $gsUser -XPOST -H "Content-type: text/xml" -T "$workDir""$layerName".xml $restSeedUrl
 
 done < $cacheList
 
